e81ad2269a7a3179e385f659c0edc1bf2d2cab14
Шарыкин Константин <kotyshe@outlook.com>
2018-11-02 00:24:52 +0200

    Created new lab #11

---
+++ Sharykin-Lab11/Sharykin-Lab11/WriteAndRead.h
@@ -0,0 +1,11 @@
+#ifndef WRITEANDREAD_H_
+#define WRITEANDREAD_H_
+#include<stdio.h>
+#include<stdlib.h>
+
+void ReadFromFile(char *nameFile, char * txt);
+void WriteToFile(char * nameFile, char * txt);
+
+
+#endif WRITEANDREAD_H_
+
---
+++ Sharykin-Lab11/Sharykin-Lab11/testLab11.cpp
@@ -0,0 +1,6 @@
+#include"WriteAndRead.h"
+
+int main()
+{
+	return 0;
+}
