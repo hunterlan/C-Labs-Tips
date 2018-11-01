65908e04315b7fbce6ae5bc71485f021614267d4
Шарыкин Константин <kotyshe@outlook.com>
2018-11-01 16:06:21 +0200

    Ended make Doxygen commentary

--- Sharykin-Lab10/Sharykin-Lab10/Tasks.h
+++ Sharykin-Lab10/Sharykin-Lab10/Tasks.h
@@ -3,6 +3,15 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
+
+/**
+* @file Tasks.h
+* Заголовчний файл Tasks
+* @author Шарикін Костянтин
+* @version 1.0
+* @date 2018.10.29
+*/
+
 void Encryption(char * txt, int countShift);
 void Decryption(char * txt, int countShift);
 void CheckPos(int size, int * pos);
