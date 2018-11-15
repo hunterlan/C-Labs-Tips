af20fc80052d92c67df890281135e2fde1ec5e22
Шарыкин Константин <kotyshe@outlook.com>
2018-11-15 08:46:25 +0200

    12345

--- Sharykin-Lab11/Sharykin-Lab11/testLab11.cpp
+++ Sharykin-Lab11/Sharykin-Lab11/testLab11.cpp
@@ -1,4 +1,22 @@
 #include"WriteAndRead.h"
+#include"WorkWithArr.h"
+
+//void DemoInsert()
+//{
+//	int posIns;
+//	printf("After which position letter we should insert a text?\n");
+//	scanf_s("%d", posIns);
+//	printf("\n");
+//	char * txt1;
+//	char * txt2;
+//	printf("Write a path to file with first text\n");
+//	scanf_s("%s", txt1);
+//	printf("\n");
+//	printf("Write a path to file with second text\n");
+//	scanf_s("%s", txt2);
+//	printf("\n");
+//	Insert(posIns, txt1, txt2);
+//}
 
 int main()
 {
